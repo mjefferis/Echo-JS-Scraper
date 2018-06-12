1480fa39587b993513f1bbb05410668f3cee6c30
Matt <mjosephjefferis@gmail.com>
2018-06-12 15:15:29 -0400

    initial file structure and config

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Echo-JS-Scraper
+# MongoScraper
+
+## About
+Users scrape articles from the Echo Js website. They can either save these articles to the webpage for future reading or delete them if not interested.
+
+## Technologies Used
+Javascript/jQuery</br>
+Node.js </br>
+Express.js</br>
+Materialize</br>
+Handlebars.js</br>
+MongoDB/Mongoose</br>
---
+++ server.js
@@ -0,0 +1,59 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+
+// Initialize Express
+var app = express();
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/echoscraper");
+
+
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+
+
+// Listen on port
+app.listen(port, function() {
+  console.log("App running on port " + port);
+});
