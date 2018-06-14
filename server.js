109df64558c98ee7a26f1f10e204bd9bd75ebdf7
Matt <mjosephjefferis@gmail.com>
2018-06-14 17:51:13 -0400

    setting up routes

--- server.js
+++ server.js
@@ -34,36 +34,103 @@ app.engine("handlebars", exphbs({
 app.set("view engine", "handlebars");
 
 //Route displaying home page with all unsaved scraped artiles
-app.get("/", function(req, res) {   
-    //Create handlebars object to help render every unsaved article on the homepage
-    var hbsObject = {
-    article: data
-    };
-    // Grab every document in the Articles collection
-    db.Article.find({"saved": false})
-    .then(function(dbArticle) {
+app.get("/", function (req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({ "saved": false })
+    .then(function (dbArticle) {
+      //Create handlebars object to help render every unsaved article on the homepage
+      var hbsObject = {
+        article: dbArticle
+      };
       //Send back unsaved articles to the client
       res.json(dbArticle);
-     
+      //render home page
+      res.render("home", hbsObject);
+
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurred, send it to the client
       res.json(err);
     });
-    //render home page
-    res.render("home", hbsObject);
 
 });
 
-//Displays saved page
-app.get("/saved", function(req, res) {   
-  res.render("saved");
-});
+// A GET route for scraping the echoJS website
+app.get("/scrape", function (req, res) {
+  // First, we grab the body of the html with request
+  axios.get("http://www.echojs.com/").then(function (response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function (i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the title and summary of every link, and save them as properties of the result object
+      result.title = $(this).children("h2").text();
+      result.summary = $(this).children(".summary").text();
+      result.link = $(this).children("h2").children("a").attr("href");
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function (dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          // If an error occurred, send it to the client
+          return res.json(err);
+        });
+    });
 
 
+    //Displays saved page
+    app.get("/saved", function (req, res) {
+      db.Article.find({ "saved": true }).populate("notes").exec(function (error, articles) {
+        var hbsObject = {
+          article: articles
+        };
+        res.render("saved", hbsObject);
+      });
+    });
 
-// Listen on port
-app.listen(port, function() {
-  console.log("App running on port " + port);
-});
+    // Save an article
+    app.post("/save/:id", function (req, res) {
+      // Use the article id to find and update its saved boolean
+      db.Article.findOneAndUpdate({ "_id": req.params.id }, { "saved": true })
+        // Execute the above query
+        .then(function (dbArticle) {
+          // Log any errors
+          if (err) {
+            console.log(err);
+          }
+          else {
+            // Or send the document to the browser
+            res.send(doc);
+          }
+        });
+    });
+
+    // Delete an article
+    app.post("/delete/:id", function (req, res) {
+      // Use the article id to find and update its saved boolean
+      db.Article.findOneAndUpdate({ "_id": req.params.id }, { "saved": false })
+        // Execute the above query
+        .exec(function (err, doc) {
+          // Log any errors
+          if (err) {
+            console.log(err);
+          }
+          else {
+            // Or send the document to the browser
+            res.send(doc);
+          }
+        });
+    });
+
+    // Listen on port
+    app.listen(port, function () {
+      console.log("App running on port " + port);
+    });
 
